27954d0cd1ab3ccb3b09aac075a9109879883c5f
KUINASI_PC\kutinasi <kutinasi6364_0126@yahoo.co.jp>
2023-08-04 12:06:58 +0900

    20230804 aws-twitter-bot.py key and token hidden.

--- aws-twitter-bot/aws-twitter-bot.py
+++ aws-twitter-bot/aws-twitter-bot.py
@@ -1,10 +1,10 @@
 import tweepy
 
 # Twitter APIキーとアクセストークン
-API_KEY = 'lwK3xV0teLNwOhN2TvHqZ5Ilj'
-API_SECRET_KEY = '8KcVkuiR6S5ZOePmiYnHGsFDIdcmvQi3WcLm6kbvXkN9eDT0gI'
-ACCESS_TOKEN = '1526133517498736640-3dmCYB7C1cNQz7ksujXRYvKt414NLm'
-ACCESS_TOKEN_SECRET = 'Fy3wY4fN6MheWLQFZfw1C0SBeUXpA1xTkFomVcYcJhhxK'
+API_KEY = ''
+API_SECRET_KEY = ''
+ACCESS_TOKEN = '-'
+ACCESS_TOKEN_SECRET = ''
 
 def lambda_handler(event, context):
     try:
